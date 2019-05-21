f4164f99a84c5a0e9afd68a0c95d7dd17319acfe
karim metawea <karimmetawea@karims-MacBook-Air.local>
2019-05-21 04:56:22 +0200

    update campaign added

--- index.js
+++ index.js
@@ -1,81 +1,26 @@
-const Joi = require('joi');
-const express =  require('express');
+const express = require('express');
 const app = express();
+const campaign = require('./campaign');
 
 app.use(express.json());
 
-const campaigns = [{name:'n1',country:'EGY',budget:149,goal:'awarness',category:'technology'},
-{name:'n2',country:'EGY',budget:149,goal:'awarness',category:'technology'}
-,{name:'n3',country:'EGY',budget:149,goal:'awarness',category:'technology'}
-,{name:'n4',country:'EGY',budget:149,goal:'awarness',category:'technology'}
-,{name:'n5',country:'EGY',budget:149,goal:'awarness',category:'technology'}]
 
-
-app.get('/api/campaigns',(req,res)=>{
-    res.send(campaigns)
+app.get('/api/campaigns', (req, res) => {
+    campaign.getCampagins(req, res);
 });
 
-app.get('/api/campaigns/:name',(req,res)=>{
- const campaign = campaigns.find(c => c.name === req.params.name)
- if(!campaign) res .status(404).send("the campaign doesn't exist")
- res.send(campaign)
+app.get('/api/campaigns/:name', (req, res) => {
+    campaign.getCampagin(req, res);
 });
 
-app.post('/api/campaigns',(req,res) => {
-const schema = {
-name: Joi.string().required(),
-country: Joi.string().required(),
-budget: Joi.number().integer().required(),
-goal: Joi.string().required(),
-category: Joi.optional()
-};
-
-const result = Joi.validate(req.body , schema)
-if (result.error){
-    res.status(400).send(result.error.details[0].message);
-    return;
-}
-const campaign = {
-    name: req.body.name
-    ,country: req.body.country
-    ,budget: req.body.budget
-    ,goal: req.body.goal
-    ,category: req.body.category
-}
-
-campaigns.push(campaign);
-res.send(campaign)
+app.post('/api/campaigns', (req, res) => {
+   campaign.createCampaign(req,res);
 });
 
-app.put('/api/campaigns/:name',(req,res)=>{
-    const campaign = campaigns.find(c => c.name === req.params.name)
-    if(!campaign) res .status(404).send("the campaign doesn't exist")
-
- const schema = {
-        name: Joi.string().required(),
-        country: Joi.string().required(),
-        budget: Joi.number().integer().required(),
-        goal: Joi.string().required(),
-        category: Joi.optional()
-    };
-
-const result = Joi.validate(req.body , schema)
-
- if (result.error){
-            res.status(400).send(result.error.details[0].message);
-            return;
-        }    
-campaign.name = req.body.name
-campaign.budget = req.body.budget
-campaign.category = req.body.category
-campaign.goal = req.body.goal
-campaign.country = req.body.country
-res.send(campaign)
+app.put('/api/campaigns/:name', (req, res) => {
+    campaign.updateCampaign(req,res);
 });
-
-
-
 
 // PORT 
-const port =  process.env.PORT || 3000;
-app.listen(port,()=> console.log(`listening on port ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}`));
